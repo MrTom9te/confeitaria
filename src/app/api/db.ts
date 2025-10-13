557b864fe4dad311f78f18540d23929945ca6040
mr_tomate <mr_tomate2024@hotmail.com>
2025-10-13 18:36:53 -0400

    database: pseudo banco de dados porque eu to com preguiça de fazer SQL

---
+++ src/app/api/db.ts
@@ -0,0 +1,6 @@
+import { User, Product, Order } from "./types";
+
+// In a real application, you would use a proper database.
+export const users: User[] = [];
+export const products: Product[] = [];
+export const orders: Order[] = [];
